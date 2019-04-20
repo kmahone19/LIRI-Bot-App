578868919981542ed6249d55bad8ad5d9114ad59
Kevin M <kevin.mahone19@gmail.com>
2019-04-20 16:45:30 -0400

    updates and fixed issues

--- liri.js
+++ liri.js
@@ -1,72 +1,81 @@
 require("dotenv").config();
+const Spotify = require('node-spotify-api')
 var keys = require("./keys.js");
+var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
 
 var comand = process.argv[2];
 var searchTerm = process.argv[3];
 
 switch (comand) {
     case "concert-this":
-        console.log("this works!");
+        concertThis();
         break;
     case "spotify-this-song":
-        console.log("this works!");
+        spotifyThis();
         break;
     case "movie-this":
-        console.log("this works!");
+        movieThis();
         break;
     case "do-what-it-says":
-        console.log("this works!");
+        doWhatItSays();
         break;
 }
 
+// searches band and prinst venue name, location and date of show
 function concertThis() {
+
     var queryUrl = "https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp";
     axios.get(queryUrl)
         .then(function (response) {
-            console.log(response.EventData.VenueData.name);
-            console.log(response.EventData.VenueData.city, response.EventData.VenueData.region);
-            console.log(response.EventData.datetime);
+            console.log(response.data)
         })
         .catch(function (error) {
             console.log(error);
         });
 }
 
+// search song and prints artist, song name, album name, and spotify preview url 
 function spotifyThis() {
 
-    spotify
-        .search({
-            type: 'track',
-            query: searchTerm
-        })
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
+    spotify.search({
+        type: 'track',
+        query: searchTerm,
+        limit: 10,
+    }, function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log(JSON.stringify(data, null, 2));
+    });
 }
 
+// searches movie and prints title, year, IMDB and rotten tomatoes ratings, country, language, plot, and actors
 function movieThis() {
 
-    var queryURL = "http://www.omdbapi.com/?i=tt3896198&apikey=a635e7be&t="+ searchTerm
-    axios.get(queryURL)
+    var queryUrl = "http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrl)
         .then(function (response) {
             var data = response.data;
-            console.log(data.title);
-            console.log(data.Year);
-            console.log(data.IMDBrating);
-            console.log(data.rating);
-            console.log(data.country);
-            console.log(data.plot);
-            console.log(data.actors);
+            console.log(data);
         })
         .catch(function (error) {
             console.log(error);
         });
 }
 
+// reads the random.txt and runs what ever comand and search term are in it 
 function doWhatItSays() {
+    fs.readFile("movies.txt", "utf8", function (error, data) {
 
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(",");
+
+        console.log(dataArr);
+
+    });
 }
